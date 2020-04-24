82fea208ba951cf4533f0284d5fad492dfbf4184
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-04-24 09:54:27 -0400

    update

--- Code_eco_modelling.R
+++ Code_eco_modelling.R
@@ -627,7 +627,7 @@ c
 plot(c)
 
 # Put observations in groups
-# Need to specify either k = groups or h = height
+# Need to specify either k = groups or h = height like h =230
 g3 <- cutree(c, k = 3)  # "g3" = "groups 3"
 # cutree(hcmt, h = 230) will give same result
 g3
@@ -647,7 +647,7 @@ km <- kmeans(mtcars1, 3)
 km
 
 # Graph based on k-means
-require(cluster)
+require(cluster)# the cluster means are centroids
 clusplot(mtcars1,  # data frame
          km$cluster,  # cluster data
          color = TRUE,  # color
@@ -659,11 +659,70 @@ rm(list = ls())  # Clean up
 
 
 
+################# Principle Component Analysis and Factor Analysis ############
+
+# PCA
+# From "psych" package documentation (p. 213)
+# "The primary empirical difference between a components 
+# versus a factor model is the treatment of the variances
+# for each item. Philosophically, components are weighted
+# composites of observed variables while in the factor
+# model, variables are weighted composites of the factors."
 
 
+# Load data 
+?mtcars
+data(mtcars)
+mtcars[1:5, ]
+mtcars1 <- mtcars[, c(1:4, 6:7, 9:11)]  # Select variables
+mtcars1[1:5, ]
 
 
+# Principle components model using default method
+# If using entire data frame:
+pc <- prcomp(mtcars1,
+             center = TRUE,  # Centers means to 0 (optional)
+             scale = TRUE)  # Sets unit variance (helpful)
+# Or specify variables:
+# pc <- prcomp(~ mpg + cyl + disp + hp + wt + qsec + am + 
+#                gear + carb, data = mtcars, scale = TRUE)
+?prcomp  # Generally preferred
+?princomp  # Very slightly different method, similar to S
 
+# Get summary stats
+summary(pc) # they have decending explainitory capabilities 
+
+# Screeplot
+plot(pc)
+
+# Get standard deviations and how variables load on PCs
+pc
+#so can see PC1 has alot to do with size of car
+#pc 2 has alot to do with performace
+#can tell from high valubles - or + for var in PC
+
+
+# See how cases load on PCs
+predict(pc)
+
+# Biplot
+biplot(pc)
+
+# Factor Analysis
+# Varimax rotation by default
+# Gives chi square test that number of factors
+# is sufficient to match data (want p > .05).
+# Also gives uniqueness values for variables,
+# variable loadings on factors, and variance
+# statistics.
+factanal(mtcars1, 1)
+factanal(mtcars1, 2)
+factanal(mtcars1, 3)
+factanal(mtcars1, 4)  # First w/p > .05
+
+rm(list = ls())  # Clean up
+
+?plot
 
 
 ############## Linear Mixed Models #####################
